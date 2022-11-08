046a378c0d55a76a3446a76a4be4d383434fa09a
Pavithran2 <pavithrank02@gmail.com>
2022-11-08 10:05:06 +0530

    local storage value retireval failed

--- login-page/src/components/SignUp.js
+++ login-page/src/components/SignUp.js
@@ -1,22 +1,49 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Home from './Home'
 import './SignIn.css'
 
 function SignUp() {
+  const [initialValues, setInitialValues] = useState({
+    username: "",
+    email: "",
+    password: "",
+    dob: ""
+  });
+  const [formValues, setFormValues] = useState([]);
+
+  const submitForm = () => {
+    setFormValues((prevFormValues) => [...prevFormValues, initialValues]);
+  };
+  useEffect(() => {
+    localStorage.setItem("formValues", JSON.stringify(formValues));
+  }, [formValues]);
+
   return (
     <div>
       <Home />
       <div >
         <form className='signin'>
         <p> Username</p>
-        <input type="text" required=" "/>
+        <input type="text" value={initialValues.username}
+          onChange={(e) =>
+            setInitialValues({ ...initialValues, username: e.target.value })
+          } />
         <p> Password</p>
-        <input type="text" required=" "/>
+        <input type="text" value={initialValues.password}
+          onChange={(e) =>
+            setInitialValues({ ...initialValues, password: e.target.value })
+          }/>
         <p> Email</p>
-        <input type="text" required=" " />
+        <input type="text" value={initialValues.email}
+          onChange={(e) =>
+            setInitialValues({ ...initialValues, email: e.target.value })
+          }/>
         <p> Date of Birth</p>
-        <input type="text" required=" "/>
-        <button>Submit</button>
+        <input type="text" value={initialValues.dob}
+          onChange={(e) =>
+            setInitialValues({ ...initialValues, dob: e.target.value })
+          }/>
+        <button onClick={submitForm}>Submit</button>
         </form>
       </div>
     </div>
