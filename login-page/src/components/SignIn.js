e0d4606e82d4dee7fa67d3433d789a40afc483b0
Pavithran2 <pavithrank02@gmail.com>
2022-11-22 11:54:54 +0530

    input retrival issue

--- login-page/src/components/SignIn.js
+++ login-page/src/components/SignIn.js
@@ -9,47 +9,44 @@ import Home from './Home';
 import SuccessCard from './SuccessCard';
 
 function SignIn() {
+
   const [initialValues, setInitialValues] = useState({
     username: "",
     password: "",
   });
-  const [inputValues, setInputValues] = useState([]);
+  const getInputValue = event => {
+    setInitialValues(event.target.value);
+    console.log(val);
+  }
+  // const [inputValues, setInputValues] = useState([]);
   const submitButton = () => {
-    setInputValues((prevInputValues) => [...prevInputValues, initialValues]);
-    formVal();
+    console.log("done");
   }
   
-  useEffect(() => {
-    localStorage.setItem("inputValues", JSON.stringify(inputValues));
+  // useEffect(() => {
+  //   localStorage.setItem("inputValues", JSON.stringify(inputValues));
     
-  }, [inputValues]);
+  // }, [inputValues]);
 
-  const formVal = () => {
-    const val = localStorage.getItem("formValues", JSON.stringify(formValues));
-    // console.log("values", inputValues);
-    if (val !== inputValues) {
-      console.log("done");
-    }
-  }
- 
-
+  // const formVal = () => {
+  //   const val = localStorage.getItem("formValues", JSON.stringify(formValues));
+  //   // console.log("values", inputValues);
+  //   if (val !== inputValues) {
+  //     console.log("done");
+    // }
   return (
     <div>
       <Home />
       <div className='signin'>
-
         <IconButton>
           <AccountCircleRoundedIcon sx={{ fontSize: 70 }} />
         </IconButton>
         <p> Username</p>
-        <input type="text" value={initialValues.username}
-          onChange={(e) =>
-            setInitialValues({ ...initialValues, username: e.target.value })
-          } />
+        <input type="text" 
+          onChange={getInputValue} />
         <p> Password</p>
         <input type="text" value={initialValues.password}
-          onChange={(e) =>
-            setInitialValues({ ...initialValues, password: e.target.value })
+          onChange={getInputValue
           } />
        { <button onClick={submitButton}>Login</button> }
       </div>
